464607ff02b39610f42f318b4a0756d89414e80e
Glydewell Burdick <glydewellb@gmail.com>
2026-05-21 16:52:26 -0500

    rename transaction sync job & tests

--- spec/jobs/ynab_transaction_sync_job_spec.rb
+++ spec/jobs/ynab_transaction_sync_job_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe YnabSyncJob, type: :job do
+RSpec.describe YnabTransactionSyncJob, type: :job do
   describe '#perform' do
     let(:plan_id) { 'fake-plan-id' }
     let(:mock_client) { instance_double(YNAB::API) }
     let(:mock_transactions_api) { instance_double(YNAB::TransactionsApi) }
-
     let(:mock_service) { instance_double(Ynab::ClientService, client: mock_client) }
 
     before do
@@ -14,9 +13,11 @@ RSpec.describe YnabSyncJob, type: :job do
     end
 
     context 'when no previous server knowledge exists' do
-      it 'fetches all transactions and saves the new server knowledge' do
+      it 'fetches all transactions, saves them, and saves the new server knowledge within a transaction' do
         mock_response = double('response', data: double('data', server_knowledge: 100, transactions: []))
         expect(mock_transactions_api).to receive(:get_transactions).with(plan_id).and_return(mock_response)
+
+        expect(ActiveRecord::Base).to receive(:transaction).and_call_original
 
         described_class.new.perform(plan_id)
 
@@ -40,6 +41,18 @@ RSpec.describe YnabSyncJob, type: :job do
 
         knowledge = ServerKnowledge.find_by(plan_id: plan_id, topic: 'transactions')
         expect(knowledge.knowledge).to eq(105)
+      end
+    end
+
+    context 'when an error occurs' do
+      it 'logs the error and re-raises it, aborting the transaction' do
+        expect(mock_transactions_api).to receive(:get_transactions).and_raise(StandardError.new('API Failure'))
+        expect(Rails.logger).to receive(:error).with(/YnabTransactionSyncJob failed for plan_id fake-plan-id: API Failure/)
+        expect(Rails.logger).to receive(:error).with(anything) # for backtrace
+
+        expect {
+          described_class.new.perform(plan_id)
+        }.to raise_error(StandardError, 'API Failure')
       end
     end
   end
